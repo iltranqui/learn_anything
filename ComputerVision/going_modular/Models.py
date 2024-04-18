ad23ba727c16adc03380691e52d9ea3518da8358
iltranqui <44050420+iltranqui@users.noreply.github.com>
2024-04-18 12:55:59 +0200

    Lazy Predict, vgg maybe doesn't work and GOogleNet Building

--- ComputerVision/going_modular/Models.py
+++ ComputerVision/going_modular/Models.py
@@ -618,11 +618,11 @@ class SegNet(nn.Module):
         Train.save_checkpoint({'epoch': epoch, 'state_dict': model.state_dict(), 'optimizer' : optimizer.state_dict()}, path)
 
 
-
+from torch import Torch
 class GoogleNet(nn.Module):
     """
     GoogleNet model implementation.
-
+    Example COde is here: https://pytorch.org/vision/main/_modules/torchvision/models/googlenet.html
     Args:
         num_classes (int): Number of output classes. Default is 10.
 
@@ -648,55 +648,196 @@ class GoogleNet(nn.Module):
         softmax (nn.Softmax): Softmax activation function.
     """
 
-    def __init__(self, num_classes=10):
+    def __init__(self, num_classes: int =10,  aux_logits: bool = True, transform_input: bool = False, dropout: float = 0.2, dropout_aux: float = 0.7):
         super(GoogleNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=192, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=192, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(in_channels=256, out_channels=480, kernel_size=3, stride=1, padding=1)
-        self.conv5 = nn.Conv2d(in_channels=480, out_channels=512, kernel_size=3, stride=1, padding=1)
-        self.conv6 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1)
-        self.conv7 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1)
-        self.conv8 = nn.Conv2d(in_channels=512, out_channels=528, kernel_size=3, stride=1, padding=1)
-        self.conv9 = nn.Conv2d(in_channels=528, out_channels=832, kernel_size=3, stride=1, padding=1)
-        self.conv10 = nn.Conv2d(in_channels=832, out_channels=832, kernel_size=3, stride=1, padding=1)
-        self.conv11 = nn.Conv2d(in_channels=832, out_channels=1024, kernel_size=3, stride=1, padding=1)
-        self.conv12 = nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(in_features=1024, out_features=1000)
-        self.fc2 = nn.Linear(in_features=1000, out_features=num_classes)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.avgpool = nn.AvgPool2d(kernel_size=7, stride=1)
-        self.dropout = nn.Dropout(0.4)
-        self.relu = nn.ReLU()
-        self.softmax = nn.Softmax()
+        self.transform_input = transform_input
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.maxpool1 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=1)
+        self.conv3 = nn.Conv2d(64, 192, kernel_size=3, padding=1)
+        self.maxpool2 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
+
+        self.inception3a = nn.Inception(192, 64, 96, 128, 16, 32, 32)
+        self.inception3b = nn.Inception(256, 128, 128, 192, 32, 96, 64)
+        self.maxpool3 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
+
+        self.inception4a = nn.Inception(480, 192, 96, 208, 16, 48, 64)
+        self.inception4b = nn.Inception(512, 160, 112, 224, 24, 64, 64)
+        self.inception4c = nn.Inception(512, 128, 128, 256, 24, 64, 64)
+        self.inception4d = nn.Inception(512, 112, 144, 288, 32, 64, 64)
+        self.inception4e = nn.Inception(528, 256, 160, 320, 32, 128, 128)
+        self.maxpool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+
+        self.inception5a = nn.Inception(832, 256, 160, 320, 32, 128, 128)
+        self.inception5b = nn.Inception(832, 384, 192, 384, 48, 128, 128)
+
+        if aux_logits:
+            self.aux1 = nn.InceptionAux(512, num_classes, dropout=dropout_aux)
+            self.aux2 = nn.InceptionAux(528, num_classes, dropout=dropout_aux)
+        else:
+            self.aux1 = None  # type: ignore[assignment]
+            self.aux2 = None  # type: ignore[assignment]
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(p=dropout_aux)
+        self.fc = nn.Linear(1024, num_classes)
 
     def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.maxpool(x)
-        x = self.relu(self.conv2(x))
-        x = self.maxpool(x)
-        x = self.relu(self.conv3(x))
-        x = self.relu(self.conv4(x))
-        x = self.relu(self.conv5(x))
-        x = self.maxpool(x)
-        x = self.relu(self.conv6(x))
-        x = self.relu(self.conv7(x))
-        x = self.relu(self.conv8(x))
-        x = self.maxpool(x)
-        x = self.relu(self.conv9(x))
-        x = self.relu(self.conv10(x))
-        x = self.relu(self.conv11(x))
-        x = self.maxpool(x)
-        x = self.relu(self.conv12(x))
+        # N x 3 x 224 x 224
+        x = self.conv1(x)
+        # N x 64 x 112 x 112
+        x = self.maxpool1(x)
+        # N x 64 x 56 x 56
+        x = self.conv2(x)
+        # N x 64 x 56 x 56
+        x = self.conv3(x)
+        # N x 192 x 56 x 56
+        x = self.maxpool2(x)
+
+        # N x 192 x 28 x 28
+        x = self.inception3a(x)
+        # N x 256 x 28 x 28
+        x = self.inception3b(x)
+        # N x 480 x 28 x 28
+        x = self.maxpool3(x)
+        # N x 480 x 14 x 14
+        x = self.inception4a(x)
+        # N x 512 x 14 x 14
+        aux1: Optional[Tensor] = None
+        if self.aux1 is not None:
+            if self.training:
+                aux1 = self.aux1(x)
+
+        x = self.inception4b(x)
+        # N x 512 x 14 x 14
+        x = self.inception4c(x)
+        # N x 512 x 14 x 14
+        x = self.inception4d(x)
+        # N x 528 x 14 x 14
+        aux2: Optional[Tensor] = None
+        if self.aux2 is not None:
+            if self.training:
+                aux2 = self.aux2(x)
+
+        x = self.inception4e(x)
+        # N x 832 x 14 x 14
+        x = self.maxpool4(x)
+        # N x 832 x 7 x 7
+        x = self.inception5a(x)
+        # N x 832 x 7 x 7
+        x = self.inception5b(x)
+        # N x 1024 x 7 x 7
+
         x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
+        # N x 1024 x 1 x 1
+        x = torch.flatten(x, 1)
+        # N x 1024
         x = self.dropout(x)
-        x = self.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        x = self.softmax(x)
+        x = self.fc(x)
+        # N x 1000 (num_classes)
+        return x, aux2, aux1
+    
+    def _transform_input(self, x: Tensor) -> Tensor:
+        if self.transform_input:
+            x_ch0 = torch.unsqueeze(x[:, 0], 1) * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
+            x_ch1 = torch.unsqueeze(x[:, 1], 1) * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
+            x_ch2 = torch.unsqueeze(x[:, 2], 1) * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
+            x = torch.cat((x_ch0, x_ch1, x_ch2), 1)
         return x
 
+class Inception(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        ch1x1: int,
+        ch3x3red: int,
+        ch3x3: int,
+        ch5x5red: int,
+        ch5x5: int,
+        pool_proj: int,
+        conv_block: Optional[Callable[..., nn.Module]] = None,
+    ) -> None:
+        super().__init__()
+        if conv_block is None:
+            conv_block = BasicConv2d
+        self.branch1 = conv_block(in_channels, ch1x1, kernel_size=1)
+
+        self.branch2 = nn.Sequential(
+            conv_block(in_channels, ch3x3red, kernel_size=1), conv_block(ch3x3red, ch3x3, kernel_size=3, padding=1)
+        )
+
+        self.branch3 = nn.Sequential(
+            conv_block(in_channels, ch5x5red, kernel_size=1),
+            # Here, kernel_size=3 instead of kernel_size=5 is a known bug.
+            # Please see https://github.com/pytorch/vision/issues/906 for details.
+            conv_block(ch5x5red, ch5x5, kernel_size=3, padding=1),
+        )
+
+        self.branch4 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1, ceil_mode=True),
+            conv_block(in_channels, pool_proj, kernel_size=1),
+        )
+
+    def _forward(self, x: Tensor) -> List[Tensor]:
+        branch1 = self.branch1(x)
+        branch2 = self.branch2(x)
+        branch3 = self.branch3(x)
+        branch4 = self.branch4(x)
+
+        outputs = [branch1, branch2, branch3, branch4]
+        return outputs
+
+    def forward(self, x: Tensor) -> Tensor:
+        outputs = self._forward(x)
+        return torch.cat(outputs, 1)
+
+
+class InceptionAux(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        num_classes: int,
+        conv_block: Optional[Callable[..., nn.Module]] = None,
+        dropout: float = 0.7,
+    ) -> None:
+        super().__init__()
+        if conv_block is None:
+            conv_block = BasicConv2d
+        self.conv = conv_block(in_channels, 128, kernel_size=1)
+
+        self.fc1 = nn.Linear(2048, 1024)
+        self.fc2 = nn.Linear(1024, num_classes)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x: Tensor) -> Tensor:
+        # aux1: N x 512 x 14 x 14, aux2: N x 528 x 14 x 14
+        x = F.adaptive_avg_pool2d(x, (4, 4))
+        # aux1: N x 512 x 4 x 4, aux2: N x 528 x 4 x 4
+        x = self.conv(x)
+        # N x 128 x 4 x 4
+        x = torch.flatten(x, 1)
+        # N x 2048
+        x = F.relu(self.fc1(x), inplace=True)
+        # N x 1024
+        x = self.dropout(x)
+        # N x 1024
+        x = self.fc2(x)
+        # N x 1000 (num_classes)
+
+        return x
+
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, **kwargs: Any) -> None:
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.conv(x)
+        x = self.bn(x)
+        return F.relu(x, inplace=True)
 
 class ResNet(nn.Module):
     """
